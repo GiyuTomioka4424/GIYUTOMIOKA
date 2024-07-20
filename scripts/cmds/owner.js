6a2d586c458daefdd9dbe547ff324c8bfb395db3
GiyuTomioka4424 <giyu4424@gmail.com>
2024-07-20 20:21:29 +0800

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -21,7 +21,7 @@ module.exports = {
 				gender: '𝗠𝗔𝗟𝗘',
 				hobby: '𝗠𝗔𝗞𝗜𝗡𝗚 𝗕𝗢𝗧𝗦',
 				Fb: 'https://facebook.com/61562362827346',
-				Relationship: '𝘄𝗶𝘁𝗵 𝘁𝗿𝗶𝘅𝗶𝗲',
+				Relationship: '𝘄𝗶𝘁𝗵 𝗺𝘆 𝗽𝗵𝗼𝗻𝗲',
 				bio: '𝗕𝗮𝗹𝗶𝗸 𝗻𝘆𝗼 𝗺𝗮𝗶𝗻 𝗮𝗰𝗰𝗼𝘂𝗻𝘁 𝗸𝗼! '
 			};
 
